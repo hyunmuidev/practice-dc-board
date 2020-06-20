0b59c164ae13451cd30701079a106456e88d4b84
hyunm <hyunm@DESKTOP-I9EHIOO>
2020-06-20 11:11:57 +0900

    mysql database & DBeaver 설치 완료

---
+++ database/Scripts/Script.sql
@@ -0,0 +1,6 @@
+-- delete from test;
+insert into `test` values (1, '홍길동', '활빈당착한놈', '2020-01-04 13:12:00', 'active');
+insert into `test` values (2, '임꺽정', '산적놈', '2020-01-05 14:12:00', 'inactive');
+insert into `test` values (3, '허균', '이야기꾼', '2020-01-04', 'active');
+
+select * from test t ;
